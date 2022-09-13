9d862734a09a5b710af7cc3dac6d2ec470b9234a
Vinicius Borges <vinicius.borges@flex.com>
2022-09-13 14:12:03 -0300

    Criada classe Posicao

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,13 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using tabuleiro;
 
 namespace xadrez_console
 {
     class Program{
         static void Main(string[] args){
+
+            // Teste criação objeto
+            Posicao P;
+            P = new Posicao(3, 4);
+
+            // Teste to string
+            Console.WriteLine("Posição: " + P);
+            Console.ReadLine();
         }
     }
 }
