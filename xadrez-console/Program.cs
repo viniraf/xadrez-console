7f090bd8fde09170aaa0b36caaa0d8b6ceb461f4
Vinicius Borges <vinicius.borges@flex.com>
2022-09-13 14:27:35 -0300

    Comecando a implementar o tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -6,13 +6,9 @@ namespace xadrez_console
     class Program{
         static void Main(string[] args){
 
-            // Teste criação objeto
-            Posicao P;
-            P = new Posicao(3, 4);
-
-            // Teste to string
-            Console.WriteLine("Posição: " + P);
-            Console.ReadLine();
+            // Teste criação tabuleiro
+            Tabuleiro tab = new Tabuleiro(8, 8);
+          
         }
     }
 }
---
+++ xadrez-console/tabuleiro/Peca.cs
@@ -0,0 +1,17 @@
+﻿namespace tabuleiro {
+    class Peca {
+
+        // Atributos
+        public Posicao posicao { get; set; }
+        public Cor cor { get; protected set; }
+        public int qteMovimentos { get; protected set; }
+        public Tabuleiro tab { get; protected set; }
+
+        // Construtor
+        public Peca (Posicao posicao, Tabuleiro tab, Cor cor) {
+            this.posicao = posicao;
+            this.tab = tab;
+            this.qteMovimentos = 0;
+        }
+    }
+}
