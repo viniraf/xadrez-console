3bd9cdd8a47db9ae3aa1d43a808434db39ca7653
Vinicius Borges <vinicius.borges@flex.com>
2022-09-15 08:41:19 -0300

    Tratando erros com excecao personalizada

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -7,17 +7,25 @@ namespace xadrez_console
     class Program{
         static void Main(string[] args){
 
-            // Teste criação tabuleiro
-            Tabuleiro tab = new Tabuleiro(8, 8);
+            try {
+                // Teste criação tabuleiro
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            // Teste colocar peca
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+                // Teste colocar peca
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
 
-            // Teste imprimir o tabuleiro
-            Tela.imprimirTabuleiro(tab);
+                // Teste exceções
+                //tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 0));
+                //tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 9));
 
+                // Teste imprimir o tabuleiro
+                Tela.imprimirTabuleiro(tab);
+            }
+            catch (TabuleiroException e) {
+                Console.WriteLine(e.Message);
+            }
             Console.ReadLine();
           
         }
--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -16,12 +16,44 @@
         public Peca peca(int linha, int coluna) {
             return pecas[linha, coluna];
         }
+        
+        // Sobrecarga metodo acessar a peça
+        public Peca peca (Posicao pos) {
+            return pecas[pos.linha, pos.coluna];
+        }
 
         // Metodo para colocar peça no tabuleiro
         public void colocarPeca(Peca p, Posicao pos) {
+            // verificação para não ser possivel colocar peça aonde já tem
+            if (existePeca(pos)) {
+                throw new TabuleiroException("Já existe uma peça nessa posição");
+            }
+
             pecas[pos.linha, pos.coluna] = p;
             p.posicao = pos;
         }
 
+
+        // Metodo para verificar se existe uma peça numa determinada posição
+        public bool existePeca(Posicao pos) {
+            validarPosicao(pos);
+            return peca(pos) != null;
+        }
+
+        // Metodo para testar se posição é valida
+        public bool posicaoValida (Posicao pos) {
+            if (pos.linha <0 || pos.linha>=linhas || pos.coluna<0 || pos.coluna >= colunas) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+
+        // Exceção
+        public void validarPosicao(Posicao pos) {
+            if (!posicaoValida(pos)) {
+                throw new TabuleiroException("Posição inválida");
+            }
+        }
     }
 }
