fec664b401863b7bd4c5ab80d2e3c840677ea97a
Vinicius Borges <vinicius.borges@flex.com>
2022-10-06 09:53:49 -0300

    Jogada especial promocao

--- xadrez-console/xadrez/PartidaDeXadrez.cs
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -138,6 +138,19 @@ namespace xadrez {
                 throw new TabuleiroException("Você não pode se colocar em xeque!");
             }
 
+            Peca p = tab.peca(destino);
+
+            // #JogadaEspecial promoção
+            if (p is Peao) {
+                if (p.cor == Cor.Branca && destino.linha == 0 || (p.cor == Cor.Preta && destino.linha == 7)) {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
             if (estaEmXeque(adversaria(jogadorAtual))) {
                 xeque = true;
             }
@@ -152,8 +165,6 @@ namespace xadrez {
                 turno++;
                 mudaJogador();
             }
-
-            Peca p = tab.peca(destino);
 
             // #JogadaEspecial en passant
             if (p is Peao && (destino.linha == origem.linha -2 || destino.linha == origem.linha + 2)) {
